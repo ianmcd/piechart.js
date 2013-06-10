8b06712ad065b85bd1fd908c8b09d49230b33ea8
ianmcd <ianmcd@gmail.com>
2013-06-10 15:49:09 -0500

    version 2.0
    handles multiple variables, added further details to sample index.html,
    updated readme

--- README.md
+++ README.md
@@ -1,21 +1,26 @@
 piechart.js
 ===========
 
-piechart.js is a very lightweight JQuery script for creating basic(single variable) css3 piecharts -- originally used in depicting the relevance of a search result.
+piechart.js is a very lightweight JQuery script for creating basic css3 piecharts -- originally used in depicting the relevance of a search result.
 
 It's very simple to use:
 
 First, style the included css file to your liking - background color / size, etc.
 
-Then create an html with the following code where you would like the piechart to be shown:
+Then create an html file with the following code(after linking the appropriate .js and .css files) where you would like the piechart to be generated:
 
-<div class="pie" title="87%"></div>
-
-The above assumes you didn't rename the css class in the stylesheet(if you have, you will need to rename it in the jquery file as well).  The title is the percent you wish to have shown.
+The above assumes you didn't rename the css class in the stylesheet(if you have, you will need to rename it in the jquery file as well). The title is the percent you wish to have shown.
 
 It's that easy.
 
-Author:		Ian McDowell
-Twitter:	@ianmcdwi
-Email:		ianmcd@gmail.com
-Web:		http://ianmcd.com
+Ian McDowell · ianmcd@gmail.com · http://ianmcd.com
+
+
+changelog
+---------
+
+2.0 (06.10.13) - handles multiple variables, added further details to sample index.html, updated readme
+
+1.1 (06.07.13) - fixed readme, further commenting to .css file
+
+1.0 (06.06.13) - initial release
--- js/piechart.js
+++ js/piechart.js
@@ -1,22 +1,37 @@
 $(function(){
 	if($(".pie")) {
 		$('.pie').each(function(i, obj) {
-			var pcent = $(this).attr("title");
-			pcent = pcent.replace(/%/g, "");
+			var pcentTot = $(this).attr("title");
+			pcentTot = pcentTot.replace(/%/g, "");
+			arrPcent = pcentTot.split(" ");
+			var pct = 0;
+			for (var i=0; i<arrPcent.length; i++) {
+				pct += Number(arrPcent[i]);
+			}
+			if (pct > 100) { 
+				var err="ERROR: percents totalling more than 100%"
+				console.log(err);
+				return err; 
+			}
 			var h = $(this).height();
 			var h2 = (h/2);
-			if (pcent > 50) {
-				h = h + "px";
-				h2 = h2 + "px";
-				var halfpcent = (pcent - 50);
-				var degrees = (360 * (halfpcent/100));
-				var newhtml = "<div style='clip: rect(0px " + h + " " + h + " 0px);'><div class='fill' style='clip: rect(0px " + h2 + " " + h + " 0px); -webkit-transform: rotate(180deg); -ms-transform: rotate(180deg); transform: rotate(180deg);'></div></div><div style='clip: rect(0px " + h + " " + h + " " + h2 + "); -webkit-transform: rotate(180deg); -ms-transform: rotate(180deg); transform: rotate(180deg);'><div class='fill' style='clip: rect(0px " + h2 + " " + h + " 0px); -webkit-transform: rotate(" + degrees + "deg); -ms-transform: rotate(" + degrees + "deg); transform: rotate(" + degrees + "deg);'></div></div>";
-				$(this).html(newhtml);
-			} else {
-				var degrees = (360 * (pcent/100));
-				var newhtml = "<div style='clip: rect(0px " + h + " " + h + " " + h2 +");'><div class='fill' style='clip: rect(0px " + h2 + " " + h + " 0px); -webkit-transform: rotate(" + degrees + "deg); -ms-transform: rotate(" + degrees + "deg); transform: rotate(" + degrees + "deg);'></div></div>";
-				$(this).html(newhtml);
+			var degreeTot = 0;
+			var newhtml = "";
+			for (var i=0; i<arrPcent.length; i++) {
+				pcent = arrPcent[i];
+				if (pcent > 50) {
+					h = h + "px";
+					h2 = h2 + "px";
+					var halfpcent = (pcent - 50);
+					var degrees = (360 * (halfpcent/100));
+					newhtml = newhtml + "<div style='clip: rect(0px " + h + " " + h + " 0px); -webkit-transform: rotate(" + degreeTot + "deg); -ms-transform: rotate(" + degreeTot + "deg); transform: rotate(" + degreeTot + "deg);'><div class='fill' style='clip: rect(0px " + h2 + " " + h + " 0px); -webkit-transform: rotate(180deg); -ms-transform: rotate(180deg); transform: rotate(180deg);'></div></div><div style='clip: rect(0px " + h + " " + h + " " + h2 + "); -webkit-transform: rotate(180deg); -ms-transform: rotate(180deg); transform: rotate(180deg);'><div class='fill" + (i + 1) + "' style='clip: rect(0px " + h2 + " " + h + " 0px); -webkit-transform: rotate(" + degrees + "deg); -ms-transform: rotate(" + degrees + "deg); transform: rotate(" + degrees + "deg);'></div></div>";
+				} else {
+					var degrees = (360 * (pcent/100));
+					newhtml = newhtml + "<div style='clip: rect(0px " + h + " " + h + " " + h2 +"); -webkit-transform: rotate(" + degreeTot + "deg); -ms-transform: rotate(" + degreeTot + "deg); transform: rotate(" + degreeTot + "deg);'><div class='fill" + (i + 1) + "' style='clip: rect(0px " + h2 + " " + h + " 0px); -webkit-transform: rotate(" + degrees + "deg); -ms-transform: rotate(" + degrees + "deg); transform: rotate(" + degrees + "deg);'></div></div>";
+				}
+				degreeTot += degrees;
 			}
+		$(this).html(newhtml);
 		});
 	}
 });
